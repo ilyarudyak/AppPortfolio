8d303633f32d0b2964c7c1f07ef2119c11159d8f
Ilya Rudyak <ilyarudyak@yahoo.com>
2015-06-10 23:26:19 +0300

    added toasts

--- app/src/main/java/com/ilyarudyak/android/appportfolio/MainActivity.java
+++ app/src/main/java/com/ilyarudyak/android/appportfolio/MainActivity.java
@@ -4,14 +4,83 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button buttonSpotifyStreamer;
+    private Button buttonScoresApp;
+    private Button buttonLibraryApp;
+    private Button buttonBuildItBigger;
+    private Button buttonXyzReader;
+    private Button buttonCapstone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSpotifyStreamer = (Button) findViewById(R.id.buttonSpotifyStreamer);
+        buttonScoresApp = (Button) findViewById(R.id.buttonScoresApp);
+        buttonLibraryApp = (Button) findViewById(R.id.buttonLibraryApp);
+        buttonBuildItBigger = (Button) findViewById(R.id.buttonBuildItBigger);
+        buttonXyzReader = (Button) findViewById(R.id.buttonXyzReader);
+        buttonCapstone = (Button) findViewById(R.id.buttonCapstone);
+
+        buttonSpotifyStreamer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.toast_spotify_streamer),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        buttonScoresApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.toast_scores_app),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        buttonLibraryApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.toast_library_app),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        buttonBuildItBigger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.toast_build_it_bigger),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        buttonXyzReader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.toast_xyz_reader),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        buttonCapstone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.toast_capstone),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
     @Override
